259963ed3d2eae22259d1a7dc92ab102caf21634
reachbp <reachpriyaa@gmail.com>
2015-12-10 00:15:51 -0500

    Update FinalLoad.java

--- src/FinalLoad.java
+++ src/FinalLoad.java
@@ -3,10 +3,6 @@ public class FinalLoad {
 public static void Load() 
 {
 	
-	//XmlParserNew xp=new XmlParserNew();
-	//xp.ParserNew();
-	//JsonLoader json=new JsonLoader();
-	//json.getJsonData();
 	JsonLoaderOrg jsonLoad=new JsonLoaderOrg();
 	jsonLoad.getJsonString();
 	System.out.println("*********LOAD COMPLETE*********");
